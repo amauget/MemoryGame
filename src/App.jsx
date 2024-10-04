c51c7b46da10bece0c0d802e84734a4c9d0d8ce6
AaronMauget <amauget@hotmail.com>
2024-10-04 10:35:45 -0700

    Add prelim styling and card logic basic format for header and body (not perfect) Basic card/deck styling add states for card order and selected status clickHandler to determine card status

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      {/* <Header></Header> */}
+      <Header></Header>
       <Cards></Cards>
     </>
   )
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './Cards.css'
 
 import accordion from './assets/accordion.jpg'
@@ -14,28 +15,30 @@ import tuba from './assets/tuba.jpg'
 import viola from './assets/viola.jpg'
 
 export default function Cards(){
-  const cardData = [
-    {name: 'Auditory Assault', src: accordion},
-    {name: 'Bass Guitar', src: bass},
-    {name: 'Cello', src: cello},
-    {name: 'Clarinet', src: clarinet},
-    {name: 'Drum Set', src: drums},
-    {name: 'Flute', src: flute},
-    {name: 'Electric Guitar', src: guitar},
-    {name: 'Piano', src: piano},
-    {name: 'Saxophone', src: sax},
-    {name: 'Trumpet', src: trumpet},
-    {name: 'Tuba', src: tuba},
-    {name: 'Viola', src: viola},
-  ]
+  const [cardSelect, updateCardSelect] = useState({
+    AuditoryAssault: false, BassGuitar: false, Cello: false, Clarinet: false, DrumSet: false, Flute: false, 
+    Guitar: false, Piano: false, Saxophone: false, Trumpet: false, Tuba: false, viola: false 
+  })
+  const [cardData, updateCardData] = useState(randomCardArray())
+
+  const handleClick = (e) =>{
+    const selected = e.target.id
+
+    if(cardSelect[selected] === false){
+      updateCardSelect(cardSelect, cardSelect[selected] = true)
+      updateCardData(randomCardArray())
+    }
+  }
   return(
     <div className='deckContainer'>
-      {cardData.map(item => {
+      {cardData.map(item => { 
         return(
           <Card
             key={item.name}
             name ={item.name}
             src = {item.src}
+            id = {(item.name).replace(' ', '')}
+            onClick={handleClick}
           ></Card> 
         )
       })}
@@ -44,13 +47,55 @@ export default function Cards(){
   )
 }
 
-function Card({ name, src }){
+function Card({ name, src, id, onClick }){
   
   return(
-    <div className='cardContainer'>
-      <img className='card' src={src}></img>
-      <h3 className='name'>{name}</h3>
+    <div className='cardContainer' onClick={onClick} id={id}>
+      <img className='card' src={src} id={id}></img>
+      <h3 className='name' id={id}>{name}</h3>
 
     </div>
   )
+}
+function randomCardArray(){
+  const items = [
+    {name: 'Auditory Assault', src: accordion},
+    {name: 'Bass Guitar', src: bass},
+    {name: 'Cello', src: cello},
+    {name: 'Clarinet', src: clarinet},
+    {name: 'Drum Set', src: drums},
+    {name: 'Flute', src: flute},
+    {name: 'Guitar', src: guitar},
+    {name: 'Piano', src: piano},
+    {name: 'Saxophone', src: sax},
+    {name: 'Trumpet', src: trumpet},
+    {name: 'Tuba', src: tuba},
+    {name: 'Viola', src: viola},
+  ]
+  let randomIndexes = randomIndexArray()
+  let randomArray = []
+
+  for(let i = 0; i < 12; i++){
+    randomArray.push(items[randomIndexes[i]])
+  }
+  return randomArray
+}
+function randomIndexArray(){
+  let array = []
+  let count = 0;
+  let index = undefined
+
+  while(count < 12){
+    index = randomNumber()
+
+    if(!array.includes(index)){
+      array.push(index)
+      count++
+    }
+  }
+  return array
+}
+
+function randomNumber(){
+  return Math.floor(Math.random() * 12)
 }
